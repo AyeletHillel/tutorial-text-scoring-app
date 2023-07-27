d38a2e58c12fb969cb33f6b93abc2301d25d7ea9
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-07-27 22:13:21 +0300

    added retry function

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import { config } from "dotenv";
 config();
 import { Configuration, OpenAIApi } from "openai";
 import { writeFile } from 'fs/promises';
+import processWithRetry from "./retryOperation.js";
 
 const API_KEY = process.env.OPEN_AI_API_KEY;
+const maxAttempts = 100;
+const delay = 1000;
 
 const openAi = new OpenAIApi(
     new Configuration({
@@ -20,4 +23,13 @@ const openAi = new OpenAIApi(
     console.log(generatedResponse);
   };
   
-  getGptResponse("How are you?");
+  const main = async (maxAttempts, delay) => {
+    const result = await processWithRetry(() => getGptResponse("How are you?"), maxAttempts, delay);
+    if (result) {
+        const generatedResponse = result.data.choices[0].message.content;
+        console.log(generatedResponse);
+    }
+};
+
+main(maxAttempts, delay);
+
