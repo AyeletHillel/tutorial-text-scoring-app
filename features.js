54d37717203877ed78e2537ad2cbd03f6a41bcbb
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-07-27 22:45:54 +0300

    added example poetry data

--- features.js
+++ features.js
@@ -4,11 +4,11 @@ export const features = [
         "prompt": "Please score the poem on its use of imagery on a 1-10 scale, 1 being minimal imagery and 10 being vivid and descriptive imagery. Your response should be in the format of a single number which is your assigned score. Poem: "
       },
       {
-        "name": "Emotional Depth",
+        "name": "Emotional_Depth",
         "prompt": "Please score the poem on its emotional depth on a 1-10 scale, 1 being not moving and 10 being deeply touching. Your response should be in the format of a single number which is your assigned score. Poem: "
       },
       {
-        "name": "Rhythm and Flow",
+        "name": "Rhythm_and_Flow",
         "prompt": "Please score the poem on its rhythm and flow on a 1-10 scale, 1 being inconsistent rhythm and 10 being a smooth and pleasing flow. Your response should be in the format of a single number which is your assigned score. Poem: "
       },
       {
@@ -16,7 +16,7 @@ export const features = [
         "prompt": "Please score the poem on its originality on a 1-10 scale, 1 being clichéd and 10 being unique and fresh. Your response should be in the format of a single number which is your assigned score. Poem: "
       },
       {
-        "name": "Coherence and Unity",
+        "name": "Coherence_and_Unity",
         "prompt": "Please score the poem on its coherence and unity on a 1-10 scale, 1 being disjointed and 10 being very coherent and unified. Your response should be in the format of a single number which is your assigned score. Poem: "
       }
     ]
