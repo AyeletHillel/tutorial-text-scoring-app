6f6175b30588eb515ff067da777df71371660859
Charlie Gottlieb <charliegottlieb@gmail.com>
2023-07-31 14:12:43 -0400

    Started work on getData

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import { config } from "dotenv";
 config();
 import { Configuration, OpenAIApi } from "openai";
 import processWithRetry from "./retryOperation.js";
+import { getData } from "./getData.js";
 import { features } from './features.js';
 
 
 const API_KEY = process.env.OPEN_AI_API_KEY;
 const maxAttempts = 10;
 const delay = 1000;
+
+const filepath = "./data/example-poetry-data.csv"
 
 const openAi = new OpenAIApi(
     new Configuration({
@@ -25,6 +28,9 @@ const openAi = new OpenAIApi(
   };
   
   const main = async (maxAttempts, delay) => {
+    const responses = await getData(filepath);
+    console.log(responses);
+
     const result = await processWithRetry(() => getGptResponse("How are you?"), maxAttempts, delay);
     if (result) {
         const generatedResponse = result.data.choices[0].message.content;
